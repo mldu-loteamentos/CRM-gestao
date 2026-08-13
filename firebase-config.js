613d00e1c0ce1dfd41cb4a33318797c367858ac2
Moura Leite <leticia@mouraleite.com>
2026-08-13 10:35:36 -0300

    v1.0.134 - 13/08/2026

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, getDocs, getDoc, addDoc, updateDoc, doc, deleteDoc, query, where, orderBy, limit, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getFirestore, collection, getDocs, getDoc, addDoc, updateDoc, doc, deleteDoc, query, where, orderBy, limit, setDoc, onSnapshot, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject, listAll } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
@@ -37,5 +37,6 @@ window.firebaseCollections = {
   uploadBytes,
   getDownloadURL,
   deleteObject,
-  listAll
+  listAll,
+  serverTimestamp
 };
